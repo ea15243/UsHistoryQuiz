dbae457dd787154b614d745c8a2933ca42debe2a
Emiliano Andrade <emilianoa027@gmail.com>
2022-08-13 13:27:25 -0400

    Html Css JavaScript

---
+++ script.js
@@ -0,0 +1,386 @@
+const questions = [
+    {
+        question: "What is the Supreme Law of the Land in The United States of America ?",
+        optionA: "The President",
+        optionB: "Emanciption Proclamation",
+        optionC: "The Bill Of Rights",
+        optionD: "The U.S. Constitution",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What are three rights of everyone living in the United States??",
+        optionA: " Freedom of religion, freedom of expression and freedom to make treaties with other countries",
+        optionB: "Freedom of speech, freedom of religion and freedom of expression",
+        optionC: "Freedom of speech, freedom of expression and freedom to run for president",
+        optionD: " Freedom to petition the government, freedom of expression and freedom to disobey traffic laws",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Who was the first President of USA ?",
+        optionA: "Donald Trump",
+        optionB: "Barack Obama",
+        optionC: "Abraham Lincoln",
+        optionD: "George Washington",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What is the responsibility that is only for U.S. Citizens",
+        optionA: "Obey the law",
+        optionB: "Be respectful of others",
+        optionC: "Pay taxes",
+        optionD: "Serve on a jury",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What territory did the U.S. Buy from France in 1803? ",
+        optionA: "Alabama",
+        optionB: "Colorado",
+        optionC: "Minnesota",
+        optionD: "Louisiana",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "The Civil War had many important events. Name one.",
+        optionA: "Emancipation Proclamation",
+        optionB: "War of 1812",
+        optionC: "Lincoln assassination",
+        optionD: "14th amendment",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Name the U.S. War between the North and the South.",
+        optionA: " The War of 1812",
+        optionB: " World War I",
+        optionC: " The Civil War",
+        optionD: "The Revolutionary War",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What U.S. war ended slavery?",
+        optionA: " The Civil War",
+        optionB: " World War I",
+        optionC: " The Revolutionary War",
+        optionD: " War of 1812",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "What was one important thing that Abraham Lincoln did?",
+        optionA: " Purchased Alaska",
+        optionB: " Declared war on Great Britain",
+        optionC: "Established the United Nations",
+        optionD: " Saved (or preserved) the Union",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What is Veterans Day?",
+        optionA: " A holiday to honor the people who died fighting for civil rights",
+        optionB: " A holiday that recognizes everyone who served in the Armed Forces",
+        optionC: " A holiday to honor the victims of 9/11",
+        optionD: " A holiday to honor soldiers who died in military service",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Name two national U.S. holidays.",
+        optionA: "Citizenship Day and Columbus Day",
+        optionB: "April Fool's Day and Labor Day",
+        optionC: "Labor Day and Thanksgiving",
+        optionD: "Valentine's Day and President's Day",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Why did the United States enter World War II? ",
+        optionA: "Japanese attacked Pearl Harbor",
+        optionB: "To get out of the Great Depression",
+        optionC: "Give the military something to do",
+        optionD: " Fight the threat of communism",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "Why did the United States enter the Persian Gulf War?",
+        optionA: "stop the spread of communism",
+        optionB: " to force the Iraqi military from Kuwait",
+        optionC: " stop the spread of Iran's influence",
+        optionD: "for its resources",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "What movement tried to end racial discrimination? ",
+        optionA: "Conservation",
+        optionB: " Prohibition",
+        optionC: "Women's suffrage",
+        optionD: " Civil rights movement",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What did the civil rights movement do? ",
+        optionA: "Fought to end racial discrimination",
+        optionB: "Gave minorities the right to vote",
+        optionC: "Gave women the right to vote",
+        optionD: "Freed the slaves",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Why did the United States enter the Korean War?",
+        optionA: "It was rich in resources",
+        optionB: "Fight the Japanese",
+        optionC: "Stop the spread of communism",
+        optionD: "Stop the spread of fascism",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Who was the United States’ main rival during the Cold War?",
+        optionA: "Soviet Union",
+        optionB: "Vietnam",
+        optionC: "Great Britain",
+        optionD: "China",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "What did Martin Luther King, Jr. do?",
+        optionA: "Ran for the president of the United States",
+        optionB: "Fought for women's suffrage",
+        optionC: "Fought for civil rights",
+        optionD: "Became a U.S. Senator",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "When did the Great Depression Start?",
+        optionA: "1920",
+        optionB: "1949",
+        optionC: "1939",
+        optionD: "1929",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Why did the United States enter the Vietnam War?",
+        optionA: "Stop the spread of communism",
+        optionB: "Fight the Japanese",
+        optionC: "Stop the spread of fascism",
+        optionD: "It was rich in resources",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "What is the name of the national anthem?",
+        optionA: "America the Beautiful",
+        optionB: "My Country Tis of Thee",
+        optionC: "God Bless the U.S.A.",
+        optionD: "The Star Bangled Banner",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Name one example of an American innovation",
+        optionA: "Skyscrapers",
+        optionB: "Light bulb",
+        optionC: "Assembly line",
+        optionD: "All of the above",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "What was the main concern of the United States during the Cold War?",
+        optionA: "Slavery",
+        optionB: "Communism",
+        optionC: "The Great Depression",
+        optionD: "Climate Change",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "What was One of the Thirteen Colonies ?",
+        optionA: "Oregon",
+        optionB: "Idaho",
+        optionC: "Georgia",
+        optionD: "California",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "How many states are in the U.S. ?",
+        optionA: "80",
+        optionB: "40",
+        optionC: "52",
+        optionD: "98",
+        correctOption: "optionC"
+    }
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1
+let playerScore = 0  
+let wrongAttempt = 0 
+let indexNumber = 0
+
+// function for displaying next question in the array to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+   
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer()
+    unCheckRadioButtons()
+    //delays next question displaying for a second
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal and resets game
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
